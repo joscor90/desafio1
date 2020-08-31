7708e586ec8447cc3e445de891f4fe93cd2b91a2
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-31 19:49:03 -0400

    added float transform using map and select elements above 5

--- desafio1.rb
+++ desafio1.rb
@@ -7,3 +7,15 @@ a = [1, 9, 2, 10, 3, 7, 4, 6]
 a1 = a.map {|e| e + 1}
 
 print "#{a1}\n"
+
+#2) Convertir todos los valores a float
+
+a2 = a.map {|e| e.to_f}
+
+print "#{a2}\n"
+
+#3) Usando select descartar todos los valores menores a 5
+
+a3 = a.select {|e| e > 5}
+
+print "#{a3}\n"
